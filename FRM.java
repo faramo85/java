c7ecdfd205b93c3b45bad00e88313445cfcac388
faramo85 <93637444+faramo85@users.noreply.github.com>
2021-11-17 06:38:34 +0300

    Add files via upload

---
+++ FRM.java
@@ -0,0 +1,21 @@
+class FRM{
+    public static void main(String [] args){
+
+
+
+
+
+
+       System.out.println             ("    **********        *******             **                  **      ");
+       System.out.println             ("    **                **      **          **  *             * **      ");
+       System.out.println             ("    **                **      **          **    *         *   **      ");
+       System.out.println             ("    **                **     **           **      *     *     **      ");
+       System.out.println             ("    ********          **  ***             **       *  *       **      ");
+       System.out.println             ("    **                **      **          **        *         **      ");
+       System.out.println             ("    **                **        **        **                  **      ");
+       System.out.println             ("    **                **         **       **                  **      ");
+       System.out.println             ("    **                **          **      **                  **      ");
+    }
+}
+    
+       
---
+++ Wordgame.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Wordgame{
+    public static void main (String []  args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter your name: ");
+        String name = input.nextLine();
+        System.out.println("enter your age: ");
+        Int age = input.nextLine();
+        System.out.println("enter your city name: ");
+        String city = input.nextLine();
+        System.out.println("enter your collage name: ");
+        String collage = input.nextLine();
+        System.out.println("enter your profession: ");
+        String profession = input.nextLine();
+        System.out.println("enter the type of your pets animal: ");
+        String animal = input.nextLine();
+
+    }
+}
